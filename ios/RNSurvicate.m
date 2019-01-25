01dd8638c93b9e9789472b4e1c997b536b4ced26
James Munro <me@jdmunro.net>
2019-01-25 15:57:09 +0000

    Add basic methods.

--- ios/RNSurvicate.m
+++ ios/RNSurvicate.m
@@ -1,4 +1,4 @@
-#import <Survicate/Survicate.h>
+@import Survicate;
 #import "RNSurvicate.h"
 
 @implementation RNSurvicate
@@ -7,7 +7,28 @@
 {
     return dispatch_get_main_queue();
 }
+
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(enterScreen:(NSString *)screenName)
+{
+    [[Survicate shared] enterScreenWithValue:screenName];
+}
+
+RCT_EXPORT_METHOD(leaveScreen:(NSString *)screenName)
+{
+    [[Survicate shared] leaveScreenWithValue:screenName];
+}
+
+RCT_EXPORT_METHOD(invokeEvent:(NSString *)eventName)
+{
+    [[Survicate shared] invokeEventWithName:eventName];
+}
+
+RCT_EXPORT_METHOD(setUserTrait:(NSString *)traitName value:(NSString *)value)
+{
+    [[Survicate shared] setUserTraitWithName:traitName value:value];
+}
 
 @end
   
